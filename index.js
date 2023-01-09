98e6b624fc86d55e316ad5c240bb6a499405849a
hieuhieu743 <anhdaden50@gmail.com>
2023-01-09 17:33:14 +0700

    sleeping

--- index.js
+++ index.js
@@ -15,10 +15,41 @@ client.on('login', () => {
 client.on('spawn', async () => {
     console.log('Spawned');
     client.chat('/gamemode creative');
-    client.creative.startFlying();
     await client.waitForTicks(1);
-    client.chat('/tp @s 204 -30 23');
+    client.chat('/tp @s 204 253 24');
 });
+
+client.on('entitySleep', (entity) => {
+    goToSleep();
+});
+
+client.on('entityWake', (entity) => {
+    wakeUp();
+});
+
+async function goToSleep() {
+    const bed = client.findBlock({
+        matching: block => client.isABed(block),
+    });
+
+    if (bed) {
+        try {
+            await client.sleep(bed);
+        } catch (err) {
+            console.error(err);
+        };
+    } else {
+        client.chat('Không thể tìm thấy block giường ở xung quanh');
+    };
+};
+
+async function wakeUp() {
+    try {
+        await client.wake();
+    } catch (err) {
+        console.error(err);
+    };
+};
 
 client.on('death', () => {
     client.emit('respawn');
