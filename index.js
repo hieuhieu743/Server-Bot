e232e5b8477295e3597ba6088028784c44b9b9fa
hieuhieu743 <anhdaden50@gmail.com>
2023-01-09 16:26:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mc = require('mineflayer');
+
+const client = mc.createBot({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    username: process.env.NAME,
+    password: process.env.PASS,
+});
+
+client.on('login', () => {
+    console.log('Connected');
+});
+
+client.on('spawn', async () => {
+    console.log('Spawned');
+    client.chat('/gamemode creative');
+    client.creative.startFlying();
+    await client.waitForTicks(1);
+    client.chat('/tp @s 204 -30 23');
+});
+
+client.on('death', () => {
+    client.emit('respawn');
+});
+
+client.on('kicked', (reason, loggedIn) => {
+    console.log(reason, loggedIn);
+});
